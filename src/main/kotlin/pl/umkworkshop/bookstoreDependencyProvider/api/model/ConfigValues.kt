b7d9766defbc60a2157d46e96dcf474296159203
Mateusz Wasieczko <mateusz.wasieczko@allegro.com>
2025-01-09 01:33:54 +0100

    set default error delay to 0

--- src/main/kotlin/pl/umkworkshop/bookstoreDependencyProvider/api/model/ConfigValues.kt
+++ src/main/kotlin/pl/umkworkshop/bookstoreDependencyProvider/api/model/ConfigValues.kt
@@ -3,11 +3,11 @@ package pl.umkworkshop.bookstoreDependencyProvider.api.model
 data class ConfigValues(
     val coreInformationDelay: Long = 100,
     val coreInformationErrorRate: Int = 0,
-    val coreInformationErrorDelay: Long = 100,
+    val coreInformationErrorDelay: Long = 0,
     val stockDelay: Long = 100,
     val stockErrorRate: Int = 0,
-    val stockErrorDelay: Long = 100,
+    val stockErrorDelay: Long = 0,
     val descriptionDelay: Long = 100,
     val descriptionErrorRate: Int = 0,
-    val descriptionErrorDelay: Long = 100
+    val descriptionErrorDelay: Long = 0
 )
